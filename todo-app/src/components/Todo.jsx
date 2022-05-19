1fd60069753c2c197fea76abddea2cc1ae865b65
Kevin Grimaldi <anthgrim@icloud.com>
2022-05-19 19:32:02 -0400

    Added todo component, data, and styles

---
+++ todo-app/src/components/Todo.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Todo = ( { text, isRushed, onRush, onDelete } ) => {
+
+    const completeStyle = {
+        backgroundColor: "#339989"
+    }
+
+    return (
+        <div className="todo-item" style={isRushed ? completeStyle : null}>
+            <span>{text} {isRushed ? "- Priority" : ""}</span>
+            <div className="btn-group">
+                {!isRushed && <button title="Rush" className="custom-btn rush" onClick={onRush}>O</button>}
+                <button title="Delete" className="custom-btn delete" onClick={onDelete}>X</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
